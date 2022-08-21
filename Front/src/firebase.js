852925917cae02a2a9c037ed383c5ab1c51abbf2
Sashank Pidur Kuppuswamy <91858306+Sashank-PK@users.noreply.github.com>
2022-08-20 17:58:56 -0700

    Delete firebase.js

--- Front/src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC824I6gHdgy51DK1vzsd9x6H2DR1tA-GI",
-    authDomain: "messenger-mern-53f59.firebaseapp.com",
-    projectId: "messenger-mern-53f59",
-    storageBucket: "messenger-mern-53f59.appspot.com",
-    messagingSenderId: "303066429630",
-    appId: "1:303066429630:web:07f67814fb73b5f2f0c6ef"
-};
